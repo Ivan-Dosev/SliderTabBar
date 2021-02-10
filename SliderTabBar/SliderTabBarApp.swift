370334558509f86980213224e8ee1e5fa36373b9
Ivan Dosev <biobeehive@abv.bg>
2021-02-10 22:51:54 +0100

    Initial Commit

---
+++ SliderTabBar/SliderTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderTabBarApp.swift
+//  SliderTabBar
+//
+//  Created by Ivan Dimitrov on 10.02.21.
+//
+
+import SwiftUI
+
+@main
+struct SliderTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
